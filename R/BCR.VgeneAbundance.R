a4cd4ca4564c3f3ce0cc0b0f523cf9c4ab80835b
zff-excellent <49525103+zff-excellent@users.noreply.github.com>
2021-12-16 10:13:09 +0800

    Update BCR.VgeneAbundance.R

--- R/BCR.VgeneAbundance.R
+++ R/BCR.VgeneAbundance.R
@@ -43,7 +43,7 @@ BCR.VgeneAbundance <- function(contigList, sampleName) {
       ggplot(aes(x = vgene, y = frequency, fill = frequency)) +
       geom_bar(stat = "identity") +
       scale_y_continuous(expand = c(0, 0)) +
-      labs(title = sampleNames, x = "TRA+TRB V Gene", y = "Barcode Frequency") +
+      labs(title = sampleNames, x = "IGH-IGK/IGL V Gene", y = "Barcode Frequency") +
       geom_text(aes(label = frequency), size = 4, vjust = 1.2, hjust = 0.5) +
       scale_fill_gradientn(colours = c("#ffffcc", colorRampPalette(c("#aedfb7", "#3799bb", "#3086b5", "#2a74af", "#225fa8"))(800))) +
       theme_bw() +
